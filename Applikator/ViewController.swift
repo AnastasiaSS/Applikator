e9fba91351434535a0f1e444761f100b32c42594
Анастасия Соколан <anastasiasokolan@macbook-air-anastasia.local>
2019-02-08 00:10:33 +0200

    Initial Commit

---
+++ Applikator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Applikator
+//
+//  Created by Анастасия Соколан on 2/8/19.
+//  Copyright © 2019 Анастасия Соколан. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
